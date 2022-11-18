918bcc4e27692e7c761206cd96b72c23c8057bb7
ChloeL6 <yenchi.le1199@gmail.com>
2022-11-18 14:01:20 -0800

    Pass all test in pytest individually

--- test_remove_dups.py
+++ test_remove_dups.py
@@ -1,23 +1,42 @@
 import pytest
 
-no_dups = []
+
 def remove_dups(sample):
-    for x in sample:
-        if x not in no_dups:
-          no_dups.append(x)
-    return no_dups
+    no_dups = []
+    if type(sample) is list and len(sample) != 0:
+        for x in sample:
+            if x not in no_dups:
+              no_dups.append(x)
+        return no_dups
+    else:
+        return None
+
    
-
-
 test1 = [1,2,3,4,4,3,2,1]
-# test2 = ["h", "l", "m", "t","m","h","t"]
-# test3 = [1,2,3,4,4,3,2,1,"h", "l", "m", "t","m","h","t"]
+test2 = ["a", "b", "c", "d", "d", "a"]
+test3 = [1,2,3,4,4,3,2,1,"h", "l", "m", "t","m","h","t"]
+test4 = []
+test5 = "astr"
 
+def test_list_num():
+    assert remove_dups(test1) == [1,2,3,4]
 
-@pytest.mark.parametrize("sample, list_no_dups", [
-    (test1, [1,2,3,4]),
-    # (test2, ["h", "l", "m", "t"])
-])
+def test_list_str():
+    assert remove_dups(test2) == ["a", "b", "c", "d"]
 
-def test_remove_dups(sample, list_no_dups):
-    assert remove_dups(sample) == list_no_dups
+def test_list_str_num():
+    assert remove_dups(test3) == [1, 2, 3, 4, "h", "l", "m", "t"]
+
+def test_empty_list():
+    assert remove_dups(test4) == None
+
+def test_str():
+    assert remove_dups(test5) == None
+
+# @pytest.mark.parametrize("sample, list_no_dups", [
+#     (test1, [1,2,3,4]),
+#     (test2, ["h", "l", "m", "t"])
+# ])
+
+# def test_remove_dups(sample, list_no_dups):
+#     assert remove_dups(sample) == list_no_dups
